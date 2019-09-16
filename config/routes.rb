64b4cea660eb6627927d8fd0bdf030dd00578c72
nath <nath@localhost.localdomain>
2019-09-16 00:10:03 +0000

    fixed routes for now, and made a seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :problem
   resources :home
 #  get 'problem/index'
-#  root 'problem#index'
-#  post 'problem/create'
-#  get 'home/index'
+  root 'problem#index'
+   post 'home/create'
+#   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Problem.create(detail: 'this is problem 1', category: 'P1')
+Problem.create(detail: 'this is problem 2', category: 'P2')
+Problem.create(detail: 'this is problem 3', category: 'P3')
+Problem.create(detail: 'this is problem 4', category: 'P4')
+Problem.create(detail: 'this is problem 5', category: 'P5')
+Problem.create(detail: 'this is problem 6', category: 'P6')
+Problem.create(detail: 'this is problem 7', category: 'P7')
+Problem.create(detail: 'this is problem 8', category: 'P8')
+
+User.create(name: 'Bob', age: '30')
+User.create(name: 'Billy', age: '40')
+User.create(name: 'Fred', age: '55')
+User.create(name: 'Sid', age: '66')
+
